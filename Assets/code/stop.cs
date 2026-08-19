6ccc34849022718cf8bd7060995fcc2da40fe94d
tanial4 <tanismejsal@gmail.com>
2026-08-19 15:57:23 -0600

    feat: add game over screen

--- Assets/code/stop.cs
+++ Assets/code/stop.cs
@@ -17,6 +17,6 @@ public class Reset : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(2);
     }
 }
